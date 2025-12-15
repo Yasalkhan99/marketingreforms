d926b6ef9e3e86bc6b53cdce0fde74eb1ff99b35
USMAN TARIQ <utariq769@gmail.com>
2025-12-15 11:50:38 +0500

    added header animation

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,12 +6,14 @@ const clashDisplay = localFont({
   src: "../public/fonts/ClashDisplay-Medium.otf",
   variable: "--font-clashDisplay",
   display: "swap",
+  preload: true,
 });
 
 const satoshiMedium = localFont({
   src: "../public/fonts/Satoshi-Medium.otf",
   variable: "--font-satoshi",
   display: "swap",
+  preload: true,
 });
 
 export const metadata: Metadata = {
@@ -26,6 +28,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Preload critical hero images for faster LCP */}
+        <link
+          rel="preload"
+          href="/images/herocircle2.png"
+          as="image"
+          fetchPriority="high"
+        />
+        <link
+          rel="preload"
+          href="/images/herocircle1.png"
+          as="image"
+        />
+        <link
+          rel="preload"
+          href="/images/herocircle3.png"
+          as="image"
+        />
+      </head>
       <body
         className={`${clashDisplay.variable} ${satoshiMedium.variable} antialiased`}
       >
